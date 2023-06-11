023219cb5662bc7d4e675d8cbde20669898aeeb3
kddco <52121443+kddco@users.noreply.github.com>
2023-06-11 20:53:02 +0800

    跨程式通訊

--- mainUI.py
+++ mainUI.py
@@ -1,11 +1,8 @@
+# 檔案A
 import tkinter as tk
 import subprocess
 import threading
-import queue
 import aes_decrypt
-
-# 定義一個 Queue 來傳遞訊息
-message_queue = queue.Queue()
 
 
 def UpdateUI(status, token, username):
@@ -15,34 +12,28 @@ def UpdateUI(status, token, username):
     ivhex = loadFromPhoneMSG.getivhexData()
     tokenvalue = aes_decrypt.aes_256_cbc_decrypt(ivhex, encrypted_hex)
     # 更改status、token和username的顯示值
-    status.set("Status: Connection")
-    token.set("Token:", tokenvalue)
-    username.set("Username: myid")
+    status.set(f"Status: Connection")
+    token.set(f"Token: {tokenvalue}")
+    username.set(f"Username: myid")
 
 
 def rfserver():
     print("rf-server running")
-    # Note: 在此處，我們需要考慮如何將 message_queue 傳遞給檔案B
     subprocess.run(['python3', 'rf-server.py'])
 
 
 def check_rfserver_status(root, status, token, username):
     try:
-        # 嘗試從 Queue 取出訊息
-        message = message_queue.get_nowait()
-        # 如果有訊息，調用 UpdateUI 函數
-        if message == "File saved":
-            UpdateUI(status, token, username)
-    except queue.Empty:
+        # 檢查通知文件
+        with open("notification.txt", "r") as file:
+            message = file.read().strip()
+            # 如果有訊息，調用 UpdateUI 函數
+            if message == "File saved":
+                UpdateUI(status, token, username)
+    except FileNotFoundError:
         pass
     # 每秒檢查一次
     root.after(1000, check_rfserver_status, root, status, token, username)
-
-
-def button_clicked():
-    print("Button Clicked")
-    # 更改username_label的text
-    username.set("Username: new_user")
 
 
 # init
@@ -61,13 +52,8 @@ status_label.pack()
 token_label = tk.Label(root, textvariable=token)
 token_label.pack()
 
-button = tk.Button(root, text="Click me", command=button_clicked)
+button = tk.Button(root, text="Click me", command=lambda: threading.Thread(target=rfserver).start())
 button.pack()
-
-# 使用 thread 執行 rfserver 函數，使其在背景運行
-rfserver_thread = threading.Thread(target=rfserver)
-rfserver_button = tk.Button(root, text="Second Button", command=rfserver_thread.start)
-rfserver_button.pack()
 
 # 開始檢查 rfserver 的狀態
 check_rfserver_status(root, status, token, username)
--- rf-server.py
+++ rf-server.py
@@ -1,14 +1,17 @@
+# 檔案B
 import bluetooth
 import threading
-import queue
 import base64
 import binascii
-import queue
 
 
 def save_string_to_file(filename, content):
     with open(filename, "w+") as file:
         file.write(content)
+    # 當文件被保存時，寫入另一個文件以通知檔案A
+    with open("notification.txt", "w") as file:
+        file.write("File saved")
+
 
 def handle_client(sock, client_info):
     print("Accepted connection from", client_info)
@@ -17,30 +20,28 @@ def handle_client(sock, client_info):
             data = sock.recv(1024)
             if not data:
                 break
-            
+
             if len(data) > 10:
                 try:
                     decoded_data = base64.b64decode(data.decode('utf-8'))  # Perform base64 decoding
-
                 except binascii.Error:
                     decoded_data = data  # Use original data if decoding fails
-                
+
                 decoded_data_str = str(decoded_data)[2:-1]  # Convert to string and remove leading "b'" and trailing "'"
                 print("Received", decoded_data_str)
                 filename = "FromPhoneMSG.txt"
                 save_string_to_file(filename, decoded_data_str)
-                message_queue.put("File saved")
 
-                
             else:
                 data_str = data.decode('utf-8')  # Convert bytes to string
                 print("Received", data_str)
-                
+
     except OSError:
         pass
 
     print(f"Disconnected from {client_info}.")
     sock.close()
+
 
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 server_sock.bind(("", bluetooth.PORT_ANY))
