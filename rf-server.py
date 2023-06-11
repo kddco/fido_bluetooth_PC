6492e849ff010762479905ec52e59334e798bdec
kddco <52121443+kddco@users.noreply.github.com>
2023-06-11 19:53:45 +0800

    Update rf-server.py

--- rf-server.py
+++ rf-server.py
@@ -4,12 +4,11 @@ import queue
 import base64
 import binascii
 
-token_queue = queue.Queue()  # 全局变量，用来存储 token
+token_queue = queue.Queue()  # Global variable to store tokens
 
 def save_string_to_file(filename, content):
     with open(filename, "w+") as file:
         file.write(content)
-        file.close()
 
 def handle_client(sock, client_info):
     print("Accepted connection from", client_info)
@@ -18,17 +17,24 @@ def handle_client(sock, client_info):
             data = sock.recv(1024)
             if not data:
                 break
-            data_str = data.decode('utf-8')  # 将 bytes 转换为字符串
-            try:
-                decoded_data = base64.b64decode(data_str)  # 进行 base64 解码
-                token_queue.put(decoded_data)  # 将 decoded_data 放入队列
-            except binascii.Error:
-                decoded_data = data_str  # 直接使用原始数据
-            decoded_data_str = str(decoded_data)[2:-1]  # 转换为字符串并去除开头的"b'"和结尾的"'"
-            print("Received", decoded_data_str)
-            filename = "FromPhoneMSG.txt"
-            content = decoded_data_str
-            save_string_to_file(filename, content)
+            
+            if len(data) > 10:
+                try:
+                    decoded_data = base64.b64decode(data.decode('utf-8'))  # Perform base64 decoding
+                    token_queue.put(decoded_data)  # Put decoded_data into the queue
+                except binascii.Error:
+                    decoded_data = data  # Use original data if decoding fails
+                
+                decoded_data_str = str(decoded_data)[2:-1]  # Convert to string and remove leading "b'" and trailing "'"
+                print("Received", decoded_data_str)
+                filename = "FromPhoneMSG.txt"
+                save_string_to_file(filename, decoded_data_str)
+            else:
+                data_str = data.decode('utf-8')  # Convert bytes to string
+                print("Received", data_str)
+                filename = "FromPhoneMSG.txt"
+                save_string_to_file(filename, data_str)
+                
     except OSError:
         pass
 
@@ -47,7 +53,7 @@ while True:
     client_thread = threading.Thread(target=handle_client, args=(client_sock, client_info))
     client_thread.start()
 
-    # 在主线程中获取 token
+    # Get tokens in the main thread
     while not token_queue.empty():
         token = token_queue.get()
         print("Token from client:", token)
